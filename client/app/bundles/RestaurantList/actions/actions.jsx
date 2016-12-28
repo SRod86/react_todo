5d38a93746f8e41d9a52889ed9a484f89c037937
Steven Rodriguez <sfrodriguez86@gmail.com>
2016-12-28 13:40:16 -0700

    Use `id` instead of `index`

--- client/app/bundles/RestaurantList/actions/actions.jsx
+++ client/app/bundles/RestaurantList/actions/actions.jsx
@@ -1,14 +1,14 @@
 import {
   ADD_RESTAURANT, VISIT_RESTAURANT,
   SET_VISIBILITY_FILTER
-} from '../constants/restaurantListConstants'
+} from '../constants/restaurantListConstants';
 
 export function addRestaurant(name, street1, street2, city, state, zip) {
   return { type: ADD_RESTAURANT, name, street1, street2, city, state, zip };
 }
 
-export function visitRestaurant(index) {
-  return { type: VISIT_RESTAURANT, index };
+export function visitRestaurant(id) {
+  return { type: VISIT_RESTAURANT, id };
 }
 
 export function setVisibilityFilter(filter) {

fa37b172e612a3ee2a374cfd15766f870160c20f
Steven Rodriguez <sfrodriguez86@gmail.com>
2016-12-28 13:39:23 -0700

    Use pure function component/Restaurant

--- client/app/bundles/RestaurantList/components/Restaurant.jsx
+++ client/app/bundles/RestaurantList/components/Restaurant.jsx
@@ -1,30 +1,29 @@
 import React, { Component, PropTypes } from 'react';
 
-export default class Restaurant extends Component {
-  render () {
-    return (
-      <li
-        style={{
-          textDecoration: visited ? 'line-through' : 'none',
-          cursor: this.props.completed ? 'default' : 'pointer'
-      }}>
-        {this.props.name}
-        {this.props.street1}
-        {this.props.street2}
-        {this.props.city}
-        {this.props.state}
-        {this.props.zip}
-      </li>
-    );
-  }
+const Restaurant = ({visited, name, street1, street2, city, state, zip, onClick}) => {
+  return (
+    <li
+      style={{textDecoration: visited ? 'line-through' : 'none'}}
+      onClick={onClick} >
+      {name}
+      {street1}
+      {street2}
+      {city}
+      {state}
+      {zip}
+    </li>
+  );
 }
 
 Restaurant.propTypes = {
   name: PropTypes.string.isRequired,
   street1: PropTypes.string.isRequired,
-  street2: PropTypes.string.isRequired,
+  street2: PropTypes.string,
   city: PropTypes.string.isRequired,
   state: PropTypes.string.isRequired,
   zip: PropTypes.string.isRequired,
-  visited: PropTypes.bool.isRequired
+  visited: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired
 }
+
+export default Restaurant
